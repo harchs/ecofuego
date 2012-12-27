c6af452a54f75aa70a4b065c7062d7d244d2e4f9
Harold Rangel Charris <harold.rangel@koombea.com>
2012-12-26 19:27:44 -0500

    -m[Ref #] set up the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecofuego::Application.config.secret_token = 'b8a831d9619afaa2def37741581fda33275c3ff5ed0bb87c2408feb13c99f1be5717ab587f544f9d14e0d0f915a8f55af0e81f64ffec52baaffeccbd837c9f17'
